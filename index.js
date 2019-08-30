54c2e084d57494a1dd5e2655be9ce74eb5d06b97
Prit Thakkar <pritthakkar2.pt@gmail.com>
2019-08-30 02:35:34 -0500

    deploying Copy-paster

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('content', function(msg){
+        console.log(msg);
+        io.emit('content', msg);
+    })
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+var port = process.env.PORT || 5000; 
+http.listen(port, function(){
+    console.log('listening on *: ' + port);
+});
